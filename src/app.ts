82d44658b3ffe8c3b125a5c5c85837f5bf865686
Moath <mkaawaja5000@gmail.com>
2024-07-25 12:25:13 +0300

    modify app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
-app.use('/api', authRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/category', categoryRoutes);
